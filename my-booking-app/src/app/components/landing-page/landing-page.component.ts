1aa3b0475d457baab1c00d872773f92320dc09c3
MalinBrag <malinbragazzi@gmail.com>
2024-09-29 19:51:47 +0200

    adding formsubmit function

--- my-booking-app/src/app/components/landing-page/landing-page.component.ts
+++ my-booking-app/src/app/components/landing-page/landing-page.component.ts
@@ -1,16 +1,28 @@
 import { Component } from '@angular/core';
 import { SearchFormComponent } from '../../shared/components/search-form/search-form.component'; 
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    SearchFormComponent
+    SearchFormComponent,
+    CommonModule,
+    NgIf
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
   subtitle = 'Welcome to My Booking App';
+  recievedFormData: any = {};
+  handleFormSubmit(event: any) {
+    this.recievedFormData = event;
+  }
+
+
+
+
+
 
 }
--- my-booking-app/src/app/shared/components/search-form/search-form.component.ts
+++ my-booking-app/src/app/shared/components/search-form/search-form.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, NgForm, FormsModule } from '@angular/forms';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { formatDate } from '@angular/common';
 import { NgFor, NgIf } from '@angular/common';
 
@@ -15,7 +15,7 @@ import { NgFor, NgIf } from '@angular/common';
   styleUrl: './search-form.component.scss'
 })
 export class SearchFormComponent {
-  
+
   searchData = {
     tripType: 'one-way',
     departureDate: '',
@@ -25,20 +25,24 @@ export class SearchFormComponent {
     passengers: 1,
   }
 
+  @Output() formSubmit = new EventEmitter<any>();
+
   locations = ['Paris', 'Rome', 'Berlin', 'New York'];
 
   onSubmit() {
+    /*
     console.log(this.searchData);
     const departureDate = new Date(this.searchData.departureDate);
     const returnDate = this.searchData.returnDate ? new Date(this.searchData.returnDate) : null;
-    //expand range
+    //expand date range 2 days before and after
     const departureRange = this.getDateRange(departureDate, 2);
     const returnRange = returnDate ? this.getDateRange(returnDate, 2) : null;
 
-    console.log("Expanded Departure Date Range:", departureRange);
-    console.log("Expanded Return Date Range:", returnRange);
-
-
+    const locationFrom = this.searchData.locationFrom;
+    const locationTo = this.searchData.locationTo;
+    const passengers = this.searchData.passengers;
+*/
+    this.formSubmit.emit(this.searchData);
   }
 
   getDateRange(date: Date, range: number) {
