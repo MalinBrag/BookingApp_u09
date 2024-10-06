3a5037db50b1929ee8f1dbb717d32be370d3ceb0
MalinBrag <malinbragazzi@gmail.com>
2024-10-06 15:22:58 +0200

    setting up backend with installations and db connection files

---
+++ backend/src/config/db.ts
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb";
+
+const uri: string = process.env.MONGO_URI || '';
+const client = new MongoClient(uri);
+
+export const connect_db = async () => {
+    try {
+        await client.connect();
+        console.log('Connected to database');
+    } catch (error) {
+        console.error('Failed to connect', error);
+        process.exit(1);
+    }
+};
+
+export { client };
+
+
+
+
+
+
+
