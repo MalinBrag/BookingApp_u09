11c8b4b1b760584ab17a51b8c1e40ecd269cfe53
MalinBrag <malinbragazzi@gmail.com>
2024-10-08 13:55:04 +0200

    updating profile component

--- my-booking-app/src/app/components/user-components/profile/profile.component.ts
+++ my-booking-app/src/app/components/user-components/profile/profile.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { BreakpointService } from '../../../core/services/breakpoint.service';
+import { UserAuthenticationService } from '../../../core/services/api/user-authentication.service';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  title = 'My Pages';
+  isMobile: boolean = false;
+  isLoggedIn: boolean = false;
+  
+  constructor(
+    private router: Router,
+    private breakpoint: BreakpointService,
+    private userAuth: UserAuthenticationService,
+  ) { }
+
+  ngOnInit(): void {
+    this.breakpoint.isMobile$.subscribe(isMobile => {
+      this.isMobile = isMobile;
+    });
+
+    this.userAuth.isLoggedIn$.subscribe(isLoggedIn => {
+      this.isLoggedIn = isLoggedIn;
+    });
+  }
+
+  logout() {
+    this.userAuth.logoutUser();
+    this.isLoggedIn = false;
+    this.router.navigate(['']);
+  }
+
+
+
+
+
 
 }
