888455ab7c7609fd8ced96a8ae4a7a169efba5c6
MalinBrag <malinbragazzi@gmail.com>
2024-10-25 13:12:56 +0200

    adding nginx config

--- my-booking-app/src/environments/environment.prod.ts
+++ my-booking-app/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    api: 'http://localhost:3000/api',
+    api: 'https://backend-production-a88c.up.railway.app',
     apiKey: 'VHruKW0x5VuPcDWmNDY8DyfE9TbsVIYd',
     apiSecret: 'O6c14cs6Goa4Lgll',
 };
